24d7f99d929c3dc0a6f5836061250c1d70fc3a12
Stas R <rar.stas@gmail.com>
2019-11-14 13:15:26 +0200

    react jss

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { IProps } from './models';
+import { useStyles } from './styles';
 
 const TestComponent: React.FC<IProps> = (props) => {
 
     const [value] = React.useState(0);
+    const classes = useStyles();
 
     return(
-        <div>
+        <div className={classes.testClassName}>
             Test value state: {value} <br/>
             Test value props: {props.test} <br/>
         </div>
